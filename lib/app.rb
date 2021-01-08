5915a641684d87e7ccd308ae0897c2d0884aa2bf
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-08 13:22:46 +0000

    Fix bug with sessions not being configured properly

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
   enable :sessions
+  set :session_secret, "secret"
 
   get '/test' do
     'test page'
@@ -11,13 +12,13 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post "/name" do
-    session["name"] = params["name"]
+    session[:name] = params["name"]
     redirect "/game"
   end
 
   get "/game" do
-    @name = session["name"]
-    erb :game
+    p session[:name]
+    erb :game, :locals => { :name => session[:name] }
   end
 
   run! if app_file == $0
--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -2,7 +2,7 @@ feature "start game" do
     scenario "user can input name" do
         visit "/"
         fill_in 'name', with: 'Player_1'
-        click_on "submit"
-        expect(page).to have_content "Player_1"
+        click_on "Submit"
+        expect(page).to have_content "Welcome, Player_1"
     end
 end
